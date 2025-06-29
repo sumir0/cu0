5969ede96b10f1231e8ee8be885aecdbc884a0ad
Ramir Sultanov <ramir0.sultanov@gmail.com>
2025-06-29 21:08:24 +0300

    remade timer checks to use steady clocks
    Signed-off-by: Ramir Sultanov <ramir0.sultanov@gmail.com>

--- checks/check_cu0_async_coarse_timer.cc
+++ checks/check_cu0_async_coarse_timer.cc
@@ -5,29 +5,31 @@
 
 int main() {
   for (auto i = 0; i < 1024; i++) {
-    auto timer2ms = cu0::AsyncCoarseTimer<float, std::milli>{
+    auto timer2ms = cu0::AsyncCoarseTimer<double, std::milli>{
       std::chrono::duration<std::int64_t, std::milli>{2}
     };
-    const auto start2ms = std::chrono::high_resolution_clock::now();
+    const auto start2ms = std::chrono::steady_clock::now();
     timer2ms.launch();
     timer2ms.wait();
     const auto elapsed2ms = std::chrono::duration_cast<
         std::chrono::duration<double, std::milli>
-    >(std::chrono::high_resolution_clock::now() - start2ms);
+    >(std::chrono::steady_clock::now() - start2ms);
     std::cout << elapsed2ms << '\n';
-    assert(elapsed2ms.count() >= 1);
+    std::cout.flush();
+    assert(elapsed2ms.count() >= 2);
     assert(elapsed2ms.count() < 8);
   }
-  auto timer2s = cu0::AsyncCoarseTimer<float, std::ratio<1, 1>>{
+  auto timer2s = cu0::AsyncCoarseTimer<double, std::ratio<1, 1>>{
     std::chrono::duration<std::int64_t>{2}
   };
-  const auto start2s = std::chrono::high_resolution_clock::now();
+  const auto start2s = std::chrono::steady_clock::now();
   timer2s.launch();
   timer2s.wait();
   const auto elapsed2s = std::chrono::duration_cast<
       std::chrono::duration<double>
-  >(std::chrono::high_resolution_clock::now() - start2s);
+  >(std::chrono::steady_clock::now() - start2s);
   std::cout << elapsed2s << '\n';
+  std::cout.flush();
   assert(elapsed2s.count() >= 1);
   assert(elapsed2s.count() < 3);
 }
--- checks/check_cu0_block_coarse_timer.cc
+++ checks/check_cu0_block_coarse_timer.cc
@@ -5,27 +5,29 @@
 
 int main() {
   for (auto i = 0; i < 1024; i++) {
-    constexpr auto timer2ms = cu0::BlockCoarseTimer<float, std::milli>{
+    constexpr auto timer2ms = cu0::BlockCoarseTimer<double, std::milli>{
       std::chrono::duration<std::int64_t, std::milli>{2}
     };
-    const auto start2ms = std::chrono::high_resolution_clock::now();
+    const auto start2ms = std::chrono::steady_clock::now();
     timer2ms.launch();
     const auto elapsed2ms = std::chrono::duration_cast<
         std::chrono::duration<double, std::milli>
-    >(std::chrono::high_resolution_clock::now() - start2ms);
+    >(std::chrono::steady_clock::now() - start2ms);
     std::cout << elapsed2ms << '\n';
+    std::cout.flush();
     assert(elapsed2ms.count() >= 2);
     assert(elapsed2ms.count() < 8);
   }
-  constexpr auto timer2s = cu0::BlockCoarseTimer<float, std::ratio<1, 1>>{
+  constexpr auto timer2s = cu0::BlockCoarseTimer<double, std::ratio<1, 1>>{
     std::chrono::duration<std::int64_t>{2}
   };
-  const auto start2s = std::chrono::high_resolution_clock::now();
+  const auto start2s = std::chrono::steady_clock::now();
   timer2s.launch();
   const auto elapsed2s = std::chrono::duration_cast<
       std::chrono::duration<double>
-  >(std::chrono::high_resolution_clock::now() - start2s);
+  >(std::chrono::steady_clock::now() - start2s);
   std::cout << elapsed2s << '\n';
+  std::cout.flush();
   assert(elapsed2s.count() >= 2);
   assert(elapsed2s.count() < 3);
 }
